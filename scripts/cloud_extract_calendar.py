567391814cc50dafdad174a03041e4fc062523fa
happydeskai <lucy@happy-desk.co.uk>
2025-04-26 11:18:28 +0100

    Update cloud_extract_calendar.py

--- scripts/cloud_extract_calendar.py
+++ scripts/cloud_extract_calendar.py
@@ -20,11 +20,17 @@ SERVICE_ACCOUNT_FILE = 'service_account.json'
 
 def create_browser():
     options = uc.ChromeOptions()
-    options.add_argument('--headless')
+    # Comment out headless for now
+    # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--start-maximized")
+    options.add_argument("--remote-debugging-port=9222")
     driver = uc.Chrome(options=options)
     return driver
+
 
 def login(driver):
     username = os.environ.get("BOOKEO_USERNAME")
